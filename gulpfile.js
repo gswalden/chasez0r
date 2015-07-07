c2e8c276afcb838efb930e29325d4e333a852d2b
Greg Walden <greg.walden@makerbot.com>
2015-07-07 15:22:52 -0400

    add nodemon.json option

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,21 @@ var gulp = require('gulp'),
   nodemon = require('gulp-nodemon'),
   plumber = require('gulp-plumber');
 
-gulp.task('develop', function () {
-  nodemon({
+var config;
+try {
+  config = require('./nodemon.json');
+} catch(err) {
+  config = {
     script: 'bot.js',
     ext: 'js',
-  });
+    "env": {
+      "SLACK_TOKEN": "slack-token-here"
+    }
+  };
+}
+
+gulp.task('develop', function () {
+  nodemon(config);
 });
 
 gulp.task('default', [
